fee83aacd8c253b32f904fa5b5e3004a116042d0
hassanali-pvpn <94057248+hassanali-pvpn@users.noreply.github.com>
2023-08-30 01:12:10 +0500

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       <Counter />
       <p>
         Visit{" "}
-        <a href="https://purevpndev.fusionauth.io/oauth2/authorize?client_id=a5bd247f-9f51-40c2-8e94-8d6535e4cbf9&state=3A6BE2FD&code_challenge_method=S256&scope=offline_access&response_type=code&redirect_uri=puredome%3A%2F%2Fhome%2Fa5bd247f-9f51-40c2-8e94-8d6535e4cbf9&code_challenge=CFwdD4DGGTYE7GKafOB3VcgPR1R8ianKwLg-bNq0TqE" target="_blank">
+        <a href="https://purevpndev.fusionauth.io/oauth2/authorize?code_challenge=TRIQeKaymA_XH4pm2Vi9yn7-yvUglasPL-ZAp8zJLR0&response_type=code&redirect_uri=puredome%3A%2F%2Fhome%2Fa5bd247f-9f51-40c2-8e94-8d6535e4cbf9&scope=offline_access&code_challenge_method=S256&client_id=a5bd247f-9f51-40c2-8e94-8d6535e4cbf9&state=2F28683E" target="_blank">
           start.solidjs.com
         </a>{" "}
         to learn how to build SolidStart apps.
